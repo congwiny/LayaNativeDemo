9c30ea6d2c6d91a6dece20641ff7e86b1a1dd13b
niuchong <niuchong@360.cn>
2022-06-16 15:56:10 +0800

    init laya tester

---
+++ jni/Application.mk
@@ -0,0 +1,12 @@
+
+PLATFORM_VERSION:=4.0
+APP_STL := c++_shared
+APP_PLATFORM := android-21
+NDK_TOOLCHAIN_VERSION := 4.9
+APP_OPTIM := release
+
+#是否需要添加测试代码
+APP_PERFTEST := 1
+
+#使用硬件fp
+APP_ABI          :=armeabi-v7a arm64-v8a x86
